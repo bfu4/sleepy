ae57ffeea3094c50c293f869782695d2258e2f08
bfu4 <bella@sleeeepyti.me>
2021-02-06 03:54:02 -0500

    OOPS

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,14 +1,14 @@
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'sleepynuxt',
+    title: 'sleepy',
     htmlAttrs: {
       lang: 'en'
     },
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' }
+      { hid: 'description', name: 'description', content: 'wowowow zzZzzz' }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
