58ea754b7eb97111bb5b2fd3f84a2242d284ec1b
bfu4 <bella@sleeeepyti.me>
2021-02-14 04:23:25 -0500

    remove unneeded

--- nuxt.config.js
+++ nuxt.config.js
@@ -21,8 +21,7 @@ export default {
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
-    '~plugins/faimpl.js',
-    '~plugins/axiosimpl.js'
+    '~plugins/faimpl.js'
   ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
